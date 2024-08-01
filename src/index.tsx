1f8306c0f7fd49bc1849834f6555862b229a4934
rohit-ghosh <rohit.ghosh.0710@gmail.com>
2024-08-01 21:31:16 +0530

    index file change

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,26 @@
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import { SnackbarProvider } from "notistack";
 import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import store from "./redux/store";
-import { appInitiated, firstLoadCompleted } from "./redux/slice/appSlice";
-
-window.addEventListener("pageshow", () => {
-  if (store.getState().app.firstLoadCompleted) {
-    store.dispatch(appInitiated());
-  } else {
-    store.dispatch(firstLoadCompleted());
-  }
-});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <SnackbarProvider maxSnack={4}>
+        <App />
+      </SnackbarProvider>
+    </Provider>
   </React.StrictMode>
 );
 
